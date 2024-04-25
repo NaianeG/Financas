b301a2322a76e7497f1ec7858b304299f63c8938
NaianeG <naianegoulartoliveira@gmail.com>
2024-04-25 10:23:23 -0300

    Adicionar arquivos de projeto.

---
+++ Trabalho Rodolfo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TrabalhoRodolfo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Trabalho Rodolfo/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Dominio.Entidades;
+using Integracao;
+using Newtonsoft.Json;
+using Microsoft.Maui.Controls;
+
+namespace TrabalhoRodolfo
+{
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void CliqueBuscarInformacoes(object sender, EventArgs e)
+    {
+        string simboloAcao = campoSimbolo.Text;
+        ShareDetails shareDetails = new ShareDetails(simboloAcao);
+        await Navigation.PushAsync(shareDetails);
+        SemanticScreenReader.Announce(BuscarInformacoes.Text);
+    }
+
+        private void CampoSimbolo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(campoSimbolo.Text))
+            {
+                BuscarInformacoes.IsEnabled = false;
+            }
+            else
+            {
+                BuscarInformacoes.IsEnabled = true;
+            }
+        }
+    }
+}
